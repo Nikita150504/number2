e630077410354cd054a1c5b9cc363bb6d5984858
Nikita150504 <dongordonn@yandex.ru>
2020-11-16 17:43:26 +0300

    New day — new print

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-def main():
-    print('My first git-repo')
+import sys
+
+from PyQt5 import uic  # Импортируем uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)  # Загружаем дизайн
+        # Обратите внимание: имя элемента такое же как в QTDesigner
+
+    def run(self):
+        self.label.setText("OK")
+        # Имя элемента совпадает с objectName в QTDesigner
 
 
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
