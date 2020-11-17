5c686cbccacaaf7248b214b45d86199a9f9c413f
Nikita150504 <dongordonn@yandex.ru>
2020-11-17 11:51:10 +0300

    New day — new print

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import random
 import sys
 
 from PyQt5 import uic
+from PyQt5.QtGui import QColor, QPainter
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
@@ -8,9 +10,30 @@ class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('UI.ui', self)
+        self.pushButton.clicked.connect(self.run)
+        self.flag = False
 
     def run(self):
-        self.label.setText("OK")
+        self.flag = True
+        self.update()
+
+    def paintEvent(self, e):
+        if self.flag:
+            qp = QPainter()
+            qp.begin(self)
+            self.drawRectangles(qp)
+            qp.end()
+
+    def drawRectangles(self, qp):
+        col = QColor(0, 0, 0)
+        col.setNamedColor('#ffff00')
+        qp.setPen(col)
+        qp.setBrush(QColor(255, 255, 0))
+        razm = random.randint(1, 100)
+        x = random.randint(0, 500)
+        y = random.randint(0, 500)
+        qp.drawEllipse(x, y, razm, razm)
+        # Имя элемента совпадает с objectName в QTDesigner
 
 
 if __name__ == '__main__':
