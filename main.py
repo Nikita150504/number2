70319b1e55a333f3889f637560bbfdc2930f5386
Nikita150504 <dongordonn@yandex.ru>
2020-11-19 12:37:48 +0300

    New day — new print

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import sys
 from PyQt5 import uic
 from PyQt5.QtGui import QColor, QPainter
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from UI import Ui_MainWindow
 
 
-class MyWidget(QMainWindow):
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('UI.ui', self)
@@ -24,7 +25,6 @@ class MyWidget(QMainWindow):
             qp.end()
 
     def drawRectangles(self, qp):
-
         r = random.randint(0, 255)
         g = random.randint(0, 255)
         b = random.randint(0, 255)
